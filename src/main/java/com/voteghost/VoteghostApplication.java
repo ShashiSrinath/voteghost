c93b4c1fab7891f361eff3c789da64b024d13460
profghost <atshashisrinath@gmail.com>
2019-08-16 12:48:55 +0530

    initial commit

---
+++ src/main/java/com/voteghost/VoteghostApplication.java
@@ -0,0 +1,13 @@
+package com.voteghost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteghostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteghostApplication.class, args);
+    }
+
+}
