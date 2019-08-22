1c3a551ea4a2258b35e8fb37d29cd570f90e7129
Shashi Srinath <38592004+profghost@users.noreply.github.com>
2019-08-23 02:04:25 +0530

    Revert "Merge pull request #2 from profghost/develop"
    This reverts commit 0726f851da09bbc46ace7fe5a59de95c14831599.

--- src/main/java/com/voteghost/web/ProductController.java
+++ src/main/java/com/voteghost/web/ProductController.java
@@ -3,8 +3,6 @@ package com.voteghost.web;
 import com.voteghost.domain.Product;
 import com.voteghost.domain.User;
 import com.voteghost.repositories.ProductRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,18 +11,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import sun.nio.cs.UTF_8;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.Optional;
 
 @Controller
 public class ProductController {
-    private Logger log = LoggerFactory.getLogger(ProductController.class);
-
     @Autowired
     private ProductRepository productRepo;
 
@@ -42,24 +35,6 @@ public class ProductController {
         }
 
         return "product";
-    }
-
-    @GetMapping("product/v/{encodedProductName}")
-    public String viewProduct(@PathVariable String encodedProductName,ModelMap model,HttpServletResponse response) throws IOException {
-        try {
-            String productName = URLDecoder.decode(encodedProductName , UTF_8.defaultCharset().name());
-            Optional<Product> productOpt = productRepo.findByName(productName);
-
-            if(productOpt.isPresent()){
-                model.put("product",productOpt.get());
-            } else {
-                response.sendError(HttpStatus.NOT_FOUND.value() , "Product  not found" );
-            }
-        } catch (UnsupportedEncodingException e) {
-            log.error("There Was A Error Decoding A Product Url",e);
-        }
-
-        return "productVisitorView";
     }
 
     @PostMapping("/products")
